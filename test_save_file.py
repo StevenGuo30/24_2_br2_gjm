3256ff43ce7e85d6184ef765768a1b88c6d17d7e
StevenGuo30 <1647545809@qq.com>
2024-02-16 18:49:58 -0600

    2.16 测试了一些函数

--- test_save_file.py
+++ test_save_file.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-rot_degrees = np.array([1,2,3,4,5,6])
+rot_degrees = [1,2,3,4,5,6]
+rot_degrees = np.array(rot_degrees)
 
 with open('F:\\Soft_arm\\code\\rot_degree.txt','w') as file:
     file.writelines([str(d)+' ' for d in rot_degrees])
